439494c016dd9ae0e9746861e7a943127ff5ef22
LUCAS HENRIQUE ALVES SANTOS <lucashas27@gmail.com>
2021-06-17 21:20:40 -0300

    primeiro tweet teste

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var app = require("./config/server.js"); //Importa o arquivo server.js
 var yql = require('yql');
 var cliente = require("./config/twitter.js"); //Importa o arquivo twitter.js
 
+
 //Configura a porta disponível ou a porta 3000
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 //Configura o host disponível ou "0.0.0.0"
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-// Function
+
+
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
 });
 
-
 app.get("/", function (req, res) {
-    // Function
-    });
+    cliente.tweetar('test')
+});
